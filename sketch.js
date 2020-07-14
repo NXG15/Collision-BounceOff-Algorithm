4955962986a40ef0e9669adc7cf4f4367c379799
NXG15 <65984350+NXG15@users.noreply.github.com>
2020-07-14 12:48:22 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+function setup() {
+  createCanvas(800,600);
+
+ 
+  fixedRect = createSprite(400, 100, 50, 80); 
+  collisionRect = createSprite(400,200,80,30);
+  movingRect = createSprite(400,600,50,30);
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+
+  stillrect = createSprite(300,300,30,70);
+
+  
+  fixedRect.shapeColor = "green";
+  movingRect.shapeColor = "green"; 
+  fixedRect.debug = true; 
+  movingRect.debug = true;
+}
+
+function draw() {
+  background(0);  
+  drawSprites();
+
+    //moving rectangle - collision
+    collisionRect.x = World.mouseX;
+    collisionRect.y = World.mouseY;
+
+      bounceOff(movingRect, fixedRect);
+
+  
+
+       if(isTouching(collisionRect,stillrect)){
+
+        collisionRect.shapeColor = "blue"; 
+        stillrect.shapeColor = "red"; 
+
+       }
+       else{
+
+        stillrect.shapeColor = "green"; 
+        collisionRect.shapeColor = "green";
+
+       }
+
+ 
+}
+
